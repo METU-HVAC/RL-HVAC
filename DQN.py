c3b50249a42c27c6d5171cfded5ed5e1fedb376f
Mehmet Hekimoğlu <mehmethekimolu@gmail.com>
2024-11-19 22:10:35 +0300

    Basic training and DQN is added

---
+++ DQN.py
@@ -0,0 +1,145 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+import random
+from collections import namedtuple, deque
+import math
+# if GPU is to be used
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else
+    "mps" if torch.backends.mps.is_available() else
+    "cpu"
+)
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 512)
+        self.layer2 = nn.Linear(512, 256)
+        self.layer3 = nn.Linear(256, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+# BATCH_SIZE is the number of transitions sampled from the replay buffer
+# GAMMA is the discount factor as mentioned in the previous section
+# EPS_START is the starting value of epsilon
+# EPS_END is the final value of epsilon
+# EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+# TAU is the update rate of the target network
+# LR is the learning rate of the ``AdamW`` optimizer
+BATCH_SIZE = 64
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+class DQNAgent:
+    def __init__(self,n_observations,n_actions):
+        self.policy_net = DQN(n_observations, n_actions).to(device)
+        self.target_net = DQN(n_observations, n_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=LR, amsgrad=True)
+        self.memory = ReplayMemory(10000)
+        self.steps_done = 0
+        self.n_actions = n_actions
+        self.eps_threshold = 0
+    def store_transition(self,state,action,next_state,reward):
+        self.memory.push(state,action,next_state,reward)
+    def select_action(self,state):
+        sample = random.random()
+        self.eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+            math.exp(-1. * self.steps_done / EPS_DECAY)
+        self.steps_done += 1
+        if sample > self.eps_threshold:
+            with torch.no_grad():
+                # t.max(1) will return the largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(state).max(1).indices.view(1, 1)
+        else:
+            return torch.tensor(random.randint(0, self.n_actions - 1), device=device, dtype=torch.long).view(1, 1)
+    def choose_greedy_action(self,state):
+        with torch.no_grad():
+            return self.policy_net(state).max(1).indices.view(1, 1)
+    def optimize_model(self):
+        if len(self.memory) < BATCH_SIZE:
+            return
+        transitions = self.memory.sample(BATCH_SIZE)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                                    if s is not None])
+
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1).values
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(BATCH_SIZE, device=device)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1).values
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
+        #print the loss
+        print("loss:",loss.item())
+        
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = self.target_net.state_dict()
+        policy_net_state_dict = self.policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+        self.target_net.load_state_dict(target_net_state_dict)
---
+++ reward.py
@@ -0,0 +1,217 @@
+import gymnasium as gym
+import sinergym
+import matplotlib.pyplot as plt
+import sinergym
+import numpy as np
+import gymnasium as gym
+import matplotlib.pyplot as plt
+from sinergym.utils.rewards import LinearReward
+from typing import Any, Dict, List, Tuple, Union
+from sinergym.utils.constants import LOG_REWARD_LEVEL, YEAR
+from datetime import datetime
+
+class MyCustomReward(LinearReward):
+    def __init__(
+        self,
+        temperature_variables: List[str],
+        energy_variables: List[str],
+        range_comfort_winter: Tuple[int, int],
+        range_comfort_summer: Tuple[int, int],
+        summer_start: Tuple[int, int] = (6, 1),
+        summer_final: Tuple[int, int] = (9, 30),
+        energy_weight: float = 0.3,
+        lambda_energy: float = 1e-4,
+        lambda_temperature: float = 1.0
+    ):
+        
+        super(LinearReward, self).__init__()
+        # Name of the variables
+        self.temp_names = temperature_variables
+        self.energy_names = energy_variables
+
+        # Reward parameters
+        self.range_comfort_winter = range_comfort_winter
+        self.range_comfort_summer = range_comfort_summer
+        self.W_energy = energy_weight
+        self.lambda_energy = lambda_energy
+        self.lambda_temp = lambda_temperature
+
+        # Summer period
+        self.summer_start = summer_start  # (month,day)
+        self.summer_final = summer_final  # (month,day)
+
+        self.logger.info('Reward function initialized.')
+        self.comfort_term_arr = []
+        self.energy_term_arr = []
+    def __call__(self, obs_dict: Dict[str, Any]
+                 ) -> Tuple[float, Dict[str, Any]]:
+        """Calculate the reward function.
+
+        Args:
+            obs_dict (Dict[str, Any]): Dict with observation variable name (key) and observation variable value (value)
+
+        Returns:
+            Tuple[float, Dict[str, Any]]: Reward value and dictionary with their individual components.
+        """
+        # Check variables to calculate reward are available
+        try:
+            assert all(temp_name in list(obs_dict.keys())
+                       for temp_name in self.temp_names)
+        except AssertionError as err:
+            self.logger.error(
+                'Some of the temperature variables specified are not present in observation.')
+            raise err
+        try:
+            assert all(energy_name in list(obs_dict.keys())
+                       for energy_name in self.energy_names)
+        except AssertionError as err:
+            self.logger.error(
+                'Some of the energy variables specified are not present in observation.')
+            raise err
+        # Get the number of people in the room (occupancy)
+        people_occupant = obs_dict['people_occupant']
+        # Energy calculation
+        energy_consumed, energy_values = self._get_energy_consumed(obs_dict)
+        energy_penalty = self._get_energy_penalty(energy_values,people_occupant)
+
+        # Comfort violation calculation
+        total_temp_violation, temp_violations = self._get_temperature_violation(
+            obs_dict)
+        comfort_penalty = self._get_comfort_penalty(temp_violations)
+
+        # Weighted sum of both terms
+        reward, energy_term, comfort_term = self._get_reward(
+            energy_penalty, comfort_penalty)
+
+        reward_terms = {
+            'energy_term': energy_term,
+            'comfort_term': comfort_term,
+            'reward_weight': self.W_energy,
+            'abs_energy_penalty': energy_penalty,
+            'abs_comfort_penalty': comfort_penalty,
+            'total_power_demand': energy_consumed,
+            'total_temperature_violation': total_temp_violation
+        }
+        
+        return reward, reward_terms
+
+    def _get_energy_consumed(self, obs_dict: Dict[str,
+                                                  Any]) -> Tuple[float,
+                                                                 List[float]]:
+        """Calculate the total energy consumed in the current observation.
+
+        Args:
+            obs_dict (Dict[str, Any]): Environment observation.
+
+        Returns:
+            Tuple[float, List[float]]: Total energy consumed (sum of variables) and List with energy consumed in each energy variable.
+        """
+
+        energy_values = [
+            v for k, v in obs_dict.items() if k in self.energy_names]
+
+        # The total energy is the sum of energies
+        total_energy = sum(energy_values)
+
+        return total_energy, energy_values
+
+    def _get_temperature_violation(
+            self, obs_dict: Dict[str, Any]) -> Tuple[float, List[float]]:
+        """Calculate the total temperature violation (ºC) in the current observation.
+
+        Returns:
+            Tuple[float, List[float]]: Total temperature violation (ºC) and list with temperature violation in each zone.
+        """
+
+        month = obs_dict['month']
+        day = obs_dict['day_of_month']
+        year = YEAR
+        current_dt = datetime(int(year), int(month), int(day))
+
+        # Periods
+        summer_start_date = datetime(
+            int(year),
+            self.summer_start[0],
+            self.summer_start[1])
+        summer_final_date = datetime(
+            int(year),
+            self.summer_final[0],
+            self.summer_final[1])
+
+        if current_dt >= summer_start_date and current_dt <= summer_final_date:
+            temp_range = self.range_comfort_summer
+        else:
+            temp_range = self.range_comfort_winter
+
+        temp_values = [
+            v for k, v in obs_dict.items() if k in self.temp_names]
+        total_temp_violation = 0.0
+        temp_violations = []
+        person_count = obs_dict['people_occupant']
+        for T in temp_values:
+            if T < temp_range[0] or T > temp_range[1]:
+                temp_violation = min(
+                    abs(temp_range[0] - T), abs(T - temp_range[1]))
+                if person_count==0 :
+                    # if there is no person in the room, the comfort violation is not considered 
+                    break
+                temp_violations.append(temp_violation)
+                total_temp_violation += temp_violation
+        
+        
+        return total_temp_violation, temp_violations
+
+    def _get_energy_penalty(self, energy_values: List[float],occupancy: int) -> float:
+        """Calculate the negative absolute energy penalty based on energy values
+
+        Args:
+            energy_values (List[float]): Energy values
+
+        Returns:
+            float: Negative absolute energy penalty value
+        """
+        
+        total_energy = sum(energy_values)
+    
+        if occupancy == 0:
+            # Penalize energy consumption more heavily when the room is empty
+            energy_penalty = -2*total_energy  # Double the penalty when no people are present
+        else:
+            # Normal energy penalty
+            energy_penalty = -total_energy   
+        return energy_penalty
+
+    def _get_comfort_penalty(self, temp_violations: List[float]) -> float:
+        """Calculate the negative absolute comfort penalty based on temperature violation values
+
+        Args:
+            temp_violations (List[float]): Temperature violation values
+
+        Returns:
+            float: Negative absolute comfort penalty value
+        """
+        comfort_penalty = -sum(temp_violations)
+        return comfort_penalty
+
+    def _get_reward(self, energy_penalty: float,
+                    comfort_penalty: float) -> Tuple[float, float, float]:
+        """It calculates reward value using the negative absolute comfort and energy penalty calculates previously.
+
+        Args:
+            energy_penalty (float): Negative absolute energy penalty value.
+            comfort_penalty (float): Negative absolute comfort penalty value.
+
+        Returns:
+            Tuple[float,float,float]: total reward calculated, reward term for energy, reward term for comfort.
+        """
+        
+        energy_term = self.lambda_energy * self.W_energy * energy_penalty
+        comfort_term = self.lambda_temp * \
+            (1 - self.W_energy) * comfort_penalty
+            
+        self.comfort_term_arr.append(comfort_term)
+        self.energy_term_arr.append(energy_term)
+        #print("comfort reward:",np.mean(self.comfort_term_arr),", energy term: ",np.mean(self.energy_term_arr))
+        reward = energy_term + comfort_term
+        # print("Total reward: ",reward, "Energy term: ",energy_term, "Comfort term: ",comfort_term)
+        return reward, energy_term, comfort_term
