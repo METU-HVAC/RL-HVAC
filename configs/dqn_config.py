ca979655e3ae9bf5afde69ef6654a9806e0aeb1d
Mehmet Hekimoğlu <mehmethekimolu@gmail.com>
2024-11-19 22:36:43 +0300

    dqn is added to directory

---
+++ configs/dqn_config.py
@@ -0,0 +1,10 @@
+DQN_CONFIG = {
+    "batch_size": 64,
+    "gamma": 0.99,
+    "eps_start": 0.9,
+    "eps_end": 0.05,
+    "eps_decay": 1000,
+    "tau": 0.005,
+    "lr": 1e-4,
+    "memory_capacity": 10000
+}
